537c80971fcc8fec09ccc21f82d241630c6c4b43
sudemazra <emirdagsudemazra@gmail.com>
2025-03-03 12:08:45 +0300

    add design

---
+++ app/src/main/java/com/example/hesapmakinesi/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hesapmakinesi;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+            editText = (EditText) findViewById(R.id.sonucText);
+            editText.setShowSoftInputOnFocus(false); //klavye açılmasın diye
+        }
+
+        public void Yazdir(View view) {
+            switch (view.getId()) {
+                case R.id.buttonTemizle
+            }
+
+        }
+
+        );
+
+    }
+}
